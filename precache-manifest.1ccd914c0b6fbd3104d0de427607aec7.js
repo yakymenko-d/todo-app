5bdc5a5f6d50ef27d31f4aeb9a714300fce5dfd4
Denys Yakymenko <denys.yakym@hotelfriend.co>
2019-08-19 00:42:29 +0300

    Updates

---
+++ precache-manifest.1ccd914c0b6fbd3104d0de427607aec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ae433f4ecbaf06d3dc8785274fa039",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "b541a14acfb518179c2a",
+    "url": "/todo-app/static/css/main.3580061b.chunk.css"
+  },
+  {
+    "revision": "89c1dee689022a7e752e",
+    "url": "/todo-app/static/js/2.144fec58.chunk.js"
+  },
+  {
+    "revision": "b541a14acfb518179c2a",
+    "url": "/todo-app/static/js/main.54280684.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
